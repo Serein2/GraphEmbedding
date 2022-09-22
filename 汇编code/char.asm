8c80b18947a0a2664d869dc70a71790a28fd1062
Serein2 <2627329910@qq.com>
2022-09-22 22:49:09 +0800

    add

--- 汇编code/char.asm
+++ 汇编code/char.asm
@@ -1,19 +1,17 @@
 assume cs:code
 code segment
-s:  mov ax, bx
-    mov si, offset s  ; 转入s的地址（个人感觉这个是偏移地址）
-    mov di, offset, s0  ; s0的地址转入di
-    mov ax, cs:[si] ; 将ax中的指令
-    mov cs:[di], ax
 
-
-
-s0: nop
-    nop
+start: 
+    mov sp, 10h
+    mov ax, 0123h
+    mov ds:[0], ax
+    call word ptr ds:[0]
     
-
-
     mov ax, 4c00h
     int 21h
+s:  
+    add ax, 1
+    ret
 
 code ends
+end start
